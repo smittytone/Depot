f3f374e8e7273e2088c14a2208081a44253dded8
Tony Smith (smittytone) <tony@smittytone.net>
2026-07-16 17:18:00 +0100

    Add Pico 2 support

---
+++ firmware/pico2/main.c
@@ -0,0 +1,46 @@
+/*
+ * Depot RP2040 Bus Host Firmware - Raspberry Pi Pico
+ *
+ * @version     1.2.3
+ * @author      Tony Smith (@smittytone)
+ * @copyright   2026
+ * @licence     MIT
+ *
+ */
+
+// C
+#include <stdbool.h>
+// Pico
+#include "pico/stdlib.h"
+// Depot
+#include "pico_led.h"
+#include "../common/serial.h"
+
+
+/*
+ * ENTRY POINT
+ */
+int main(void) {
+    // Initialise the LED
+    pico_led_init();
+    pico_led_off();
+
+    // Enable STDIO and allow 2s for the board to come up
+    if (stdio_usb_init()) {
+        stdio_set_translate_crlf(&stdio_usb, false);
+        stdio_flush();
+
+        // Start the loop
+        // Function defined in `serial.c`
+        rx_loop();
+
+        // End
+        // return 0;
+    }
+
+    // Could not initialize stdio over USB,
+    // so signal error and end
+    pico_led_flash(10);
+    pico_led_on();
+    return 1;
+}
---
+++ firmware/pico2/pico_led.h
@@ -0,0 +1,37 @@
+/*
+ * Depot RP2040 Bus Host Firmware - Pi Pico LED
+ *
+ * @version     1.2.3
+ * @author      Tony Smith (@smittytone)
+ * @copyright   2026
+ * @licence     MIT
+ *
+ */
+#ifndef _HEADER_PICO_LED_
+#define _HEADER_PICO_LED_
+
+
+/*
+ * INCLUDES
+ */
+#include <stdbool.h>
+#include <stdint.h>
+
+
+/*
+ * CONSTANTS
+ */
+#define     PIN_PICO_LED            25
+
+
+/*
+ * PROTOTYPES
+ */
+void    pico_led_init(void);
+void    pico_led_off(void);
+void    pico_led_on(void);
+void    pico_led_set_state(bool is_on);
+void    pico_led_flash(uint32_t count);
+
+
+#endif  // _HEADER_PICO_LED_
